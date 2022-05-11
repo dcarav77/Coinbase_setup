55002027e4edbd5488fd4281f5ac3e9f3fa1166e
dcarav77 <57645869+dcarav77@users.noreply.github.com>
2022-05-11 11:12:15 -0500

    Create sanity.js

---
+++ sanity.js
@@ -0,0 +1,9 @@
+import { SanityClient } from "@sanity/client";
+
+export  const client = SanityClient ({
+    projectId: 'nro5cbpk'
+    dataset: 'production'
+    apiVersion: '2022-05-10'
+    token: ''
+    usecdn: false,
+)}
