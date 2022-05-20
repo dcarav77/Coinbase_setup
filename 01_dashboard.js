1569b8618f455ec80992948ef2a5679cb7b407ff
dcarav77 <57645869+dcarav77@users.noreply.github.com>
2022-05-20 07:37:21 -0500

    Create 01_dashboard.js

---
+++ 01_dashboard.js
@@ -0,0 +1,32 @@
+import React from "react"
+import styled from "styled-components"
+import Header from "./components/header"
+import Main from '/Users/dcarav77/coinbase2-blockchain-app/pages/components/main.js'
+import Sidebar from "./components/Sidebar"
+
+
+const Dashboard = ({address}) => {
+    return ( 
+    <Wrapper>
+      <Sidebar /> 
+      <MainContainer>
+        <Header walletAddress={address} /> 
+        <Main/>
+      </MainContainer>
+    </Wrapper> 
+       )
+}
+
+export default Dashboard
+ 
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #0a0b0d;
+  color: white;
+  overflow: hidden;
+`
+const MainContainer = styled.div`
+  flex: 1;
+`
